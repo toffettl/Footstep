f63b5904bc6af1217b149a64b4c2632a811cf843
toffettl <toffetti0824@gmail.com>
2025-07-07 16:25:12 -0300

    feat: test controller

---
+++ src/Footstep.Api/Controllers/TesteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Footstep.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TesteController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult Post()
+    {
+        return Ok();
+    }
+}
