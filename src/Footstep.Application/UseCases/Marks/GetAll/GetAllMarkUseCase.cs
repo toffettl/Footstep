9fcda1a2104d7a3f138d697100e0571fe11bc261
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-15 19:03:23 -0300

    feat: add GetAllMarkUseCase

---
+++ src/Footstep.Application/UseCases/Marks/GetAll/GetAllMarkUseCase.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Footstep.Application.UseCases.Marks.Get;
+using Footstep.Communication.Responses.Marks;
+using Footstep.Domain.Repositories.Marks;
+
+namespace Footstep.Application.UseCases.Marks.GetAll
+{
+    public class GetAllMarkUseCase : IGetAllMarkUseCase
+    {
+        private readonly IMarkReadOnlyRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetAllMarkUseCase(IMarkReadOnlyRepository repository,
+            IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseMarksJson> Execute()
+        {
+            var result = await _repository.GetAll();
+
+            return new ResponseMarksJson
+            {
+                Marks = _mapper.Map<List<ResponseMarkJson>>(result)
+            };
+        }
+    }
+}
