b11a66f70083a150fb28cfa1c6c07dbdab635427
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-15 19:02:07 -0300

    feat: add IGetAllMarkUseCase interface

---
+++ src/Footstep.Application/UseCases/Marks/GetAll/IGetAllMarkUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Footstep.Communication.Responses.Marks;
+using Footstep.Domain.Entities;
+
+namespace Footstep.Application.UseCases.Marks.Get
+{
+    public interface IGetAllMarkUseCase
+    {
+        Task<ResponseMarksJson> Execute();
+    }
+}
