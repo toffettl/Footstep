126215fe14cedf21f55079cb17d41c075b7097fd
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-15 19:44:10 -0300

    feat: add GetByIdMarkUseCase

---
+++ src/Footstep.Application/UseCases/Marks/GetById/GetByIdMarkUseCase.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Footstep.Communication.Responses.Marks;
+using Footstep.Domain.Repositories.Marks;
+using Footstep.Exception;
+using Footstep.Exception.ExceptionsBase;
+
+namespace Footstep.Application.UseCases.Marks.GetById
+{
+    public class GetByIdMarkUseCase : IGetByIdMarkUseCase
+    {
+        private readonly IMarkReadOnlyRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetByIdMarkUseCase(IMarkReadOnlyRepository repository,
+            IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseMarkJson> Execute(Guid id)
+        {
+            var result = await _repository.GetById(id);
+            
+            if(result == null) {
+                throw new NotFoundException(ResourceErrorMessages.MARK_NOT_FOUND);
+            }
+
+            return _mapper.Map<ResponseMarkJson>(result);
+        }
+    }
+}
