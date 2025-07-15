7aef83d043af07cd4a92c012b9db42a4167eae0e
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-15 20:14:05 -0300

    feat: add DeleteMarkUseCase

---
+++ src/Footstep.Application/UseCases/Marks/Delete/DeleteMarkUsecCase.cs
@@ -0,0 +1,32 @@
+﻿using Footstep.Domain.Repositories;
+using Footstep.Domain.Repositories.Marks;
+using Footstep.Exception;
+using Footstep.Exception.ExceptionsBase;
+
+namespace Footstep.Application.UseCases.Marks.Delete
+{
+    public class DeleteMarkUsecase : IDeleteMarkUseCase
+    {
+        private readonly IMarkWriteOnlyRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteMarkUsecase(IMarkWriteOnlyRepository repository,
+            IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Execute(Guid id)
+        {
+            var result = await _repository.Delete(id);
+
+            if (result == false)
+            {
+                throw new NotFoundException(ResourceErrorMessages.MARK_NOT_FOUND);
+            }
+
+            await _unitOfWork.Commit();
+        }
+    }
+}
