fa0e228c214c2c1af09a20df70a04e5c479bad2c
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-15 18:56:16 -0300

    feat: add IMarkReadOnlyRepository interface in MarkRepository

--- src/Footstep.Infrastructure/DataAccess/Repositories/MarkRepository.cs
+++ src/Footstep.Infrastructure/DataAccess/Repositories/MarkRepository.cs
@@ -18,6 +18,11 @@ namespace Footstep.Infrastructure.DataAccess.Repositories
             await _dbContext.Marks.AddAsync(mark);
         }
 
+        public async Task<List<Mark>> GetAll()
+        {
+            return await _dbContext.Marks.AsNoTracking().ToListAsync();
+        }
+
         public async Task<Mark?> GetById(Guid id)
         {
             return await _dbContext.Marks.AsNoTracking().FirstOrDefaultAsync(m => m.Id == id);
