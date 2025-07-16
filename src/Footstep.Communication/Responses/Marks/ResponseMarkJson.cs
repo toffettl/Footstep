85fc6484b3bbf806ab2b967eee795944daa38370
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-15 21:14:32 -0300

    feat: add Latitude and Longitude properties in ResponseMarkJson

--- src/Footstep.Communication/Responses/Marks/ResponseMarkJson.cs
+++ src/Footstep.Communication/Responses/Marks/ResponseMarkJson.cs
@@ -4,5 +4,7 @@
     {
         public Guid Id { get; set; }
         public string? Title { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
     }
 }
