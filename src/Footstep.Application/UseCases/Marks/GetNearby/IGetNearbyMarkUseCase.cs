d55d4a64485b060bd3357259a1c3b68f9cc1ecc6
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-15 21:15:02 -0300

    feat: add IGetNearbyMarkUseCase interface

---
+++ src/Footstep.Application/UseCases/Marks/GetNearby/IGetNearbyMarkUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Footstep.Communication.Responses.Marks;
+
+namespace Footstep.Application.UseCases.Marks.GetNearby
+{
+    public interface IGetNearbyMarkUseCase
+    {
+        Task<ResponseMarksJson> Execute(double latitude, double longitude, double radiusInMeters);
+    }
+}
