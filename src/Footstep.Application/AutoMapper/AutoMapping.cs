c196a1a3879c39a8333a15b9bf59303e1ebb3a33
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-20 11:06:39 -0300

    feat: add AutoMapper profile to map User to ResponseGetUserJson with Preferences and UnlockedStyles

--- src/Footstep.Application/AutoMapper/AutoMapping.cs
+++ src/Footstep.Application/AutoMapper/AutoMapping.cs
@@ -26,6 +26,31 @@ namespace Footstep.Application.AutoMapper
         {
             CreateMap<Trace, ResponseCreateTraceJson>();
             CreateMap<Trace, ResponseTraceJson>();
+            CreateMap<User, ResponseGetUserJson>()
+                .ForMember(dest => dest.Preferences, config => config.MapFrom(src => new Preferences
+                {
+                    MapStyle = src.MapStyle,
+                    PointOfInterestStyle = src.PointOfInterestStyle,
+                    AvatarOverProfile = src.AvatarOverProfile,
+                    AvatarStyle = new AvatarStyle
+                    {
+                        Head = src.HeadStyle,
+                        Torso = src.TorsoStyle,
+                        Leg = src.LegStyle,
+                        Bag = src.BagStyle,
+                        Acessory = src.AcessoryStyle,
+                    }
+                }))
+                .ForMember(dest => dest.UnlockedStyles, config => config.MapFrom(src => new UnlockedStyles
+                {
+                    UnlockedMapStyles = src.UnlockedMapStyles,
+                    UnlockedPointOfInterestStyles = src.UnlockedPointOfInterestStyles,
+                    UnlockedHeadStyles = src.UnlockedHeadStyles,
+                    UnlockedTorsoStyles = src.UnlockedTorsoStyles,
+                    UnlockedLegStyles = src.UnlockedLegStyles,
+                    UnlockedBagStyles = src.UnlockedBagStyles,
+                    UnlockedAcessoryStyles = src.UnlockedAcessoryStyles
+                }));
         }
     }
 }
