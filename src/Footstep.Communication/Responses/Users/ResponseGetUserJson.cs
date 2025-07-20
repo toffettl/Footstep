1ce57cb99096ee6f5823e3123b086b2043d4a958
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-20 11:02:54 -0300

    feat: add Preferences and UnlockedStyles properties

---
+++ src/Footstep.Communication/Responses/Users/ResponseGetUserJson.cs
@@ -0,0 +1,14 @@
+﻿namespace Footstep.Communication.Responses.Users
+{
+    public class ResponseGetUserJson
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Biography { get; set; }
+        public Preferences Preferences { get; set; }
+        public UnlockedStyles UnlockedStyles { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
