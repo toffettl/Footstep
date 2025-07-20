ab66310d0b12e84e58942a534d396c85df6fad15
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-20 11:02:28 -0300

    feat: add UnlockedStyles

---
+++ src/Footstep.Communication/Responses/Users/UnlockedStyles.cs
@@ -0,0 +1,13 @@
+﻿namespace Footstep.Communication.Responses.Users
+{
+    public class UnlockedStyles
+    {
+        public string? UnlockedMapStyles { get; set; }
+        public string? UnlockedPointOfInterestStyles { get; set; }
+        public string? UnlockedHeadStyles { get; set; }
+        public string? UnlockedTorsoStyles { get; set; }
+        public string? UnlockedLegStyles { get; set; }
+        public string? UnlockedBagStyles { get; set; }
+        public string? UnlockedAcessoryStyles { get; set; }
+    }
+}
