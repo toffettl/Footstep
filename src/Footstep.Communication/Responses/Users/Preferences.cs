b358f1ad7d3f4c8a445c0ff31edeead3a909860f
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-07-20 11:02:11 -0300

    feat: add Preferences

---
+++ src/Footstep.Communication/Responses/Users/Preferences.cs
@@ -0,0 +1,10 @@
+﻿namespace Footstep.Communication.Responses.Users
+{
+    public class Preferences
+    {
+        public string? MapStyle { get; set; }
+        public string? PointOfInterestStyle { get; set; }
+        public bool? AvatarOverProfile { get; set; }
+        public AvatarStyle AvatarStyle { get; set; }
+    }
+}
