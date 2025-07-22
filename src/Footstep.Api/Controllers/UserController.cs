6785cba7de5a85a59b351cc87d57c16bca48966e
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-07-21 21:59:59 -0300

    feat: add UpdatePreferences function in UserController

--- src/Footstep.Api/Controllers/UserController.cs
+++ src/Footstep.Api/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Footstep.Application.UseCases.Users.GetByEmail;
 using Footstep.Application.UseCases.Users.Register;
+using Footstep.Application.UseCases.Users.UpdatePreferences;
 using Footstep.Communication.Requests.Users;
 using Footstep.Communication.Responses;
 using Footstep.Communication.Responses.Users;
@@ -25,10 +26,26 @@ public class UserController : ControllerBase
     [Route("{email}")]
     [ProducesResponseType(typeof(ResponseGetUserJson), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> GetByEmail([FromServices] IGetByEmailUserUseCase useCase, [FromRoute] string email)
+    public async Task<IActionResult> GetByEmail(
+        [FromServices] IGetByEmailUserUseCase useCase, 
+        [FromRoute] string email)
     {
         var response = await useCase.Execute(email);
 
         return Ok(response);
+    }
+
+    [HttpPut]
+    [Route("preferences/{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdatePreferences(
+        [FromServices] IUpdatePreferencesUserUseCase useCase,
+        [FromRoute] Guid id,
+        [FromBody] RequestUpdatePreferencesUserJson request)
+    {
+        await useCase.Execute(id, request);
+
+        return NoContent();
     }
 }
