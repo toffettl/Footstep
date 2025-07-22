ffdbb6d1a2f88ec5e823159d3ccc19a40e8a1a4a
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-07-22 00:11:58 -0300

    feat: add RequestUpdateUnlockedStylesUserJson

---
+++ src/Footstep.Communication/Requests/Users/RequestUpdateUnlockedStylesUserJson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footstep.Communication.Requests.Users
+{
+    public class RequestUpdateUnlockedStylesUserJson
+    {
+        public string? UnlockedMapStyles { get; set; }
+        public string? UnlockedPointOfInterestStyles { get; set; }
+        public string? UnlockedHeadStyles { get; set; }
+        public string? UnlockedTorsoStyles { get; set; }
+        public string? UnlockedLegStyles { get; set; }
+        public string? UnlockedBagStyles { get; set; }
+        public string? UnlockedAcessoryStyles { get; set; }
+    }
+}
