9a6aec9037e0c42dc8ed4937e87144dae4580d73
toffettl <toffetti0824@gmail.com>
2025-07-22 15:01:46 -0300

    V1.0.20

--- src/Footstep.Infrastructure/DependencyInjectionExtension.cs
+++ src/Footstep.Infrastructure/DependencyInjectionExtension.cs
@@ -42,7 +42,7 @@ namespace Footstep.Infrastructure
 
         private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
         {
-            var conectionString = "Host=yamabiko.proxy.rlwy.net;Port=23952;Database=footstep;Username=postgres;Password=cxESXilghkzQBLYjuNtNRSuDyAjcnrVz;";
+            var conectionString = "Host=interchange.proxy.rlwy.net;Port=37540;Database=footstep;Username=postgres;Password=MSYYgFCfUyXFFLJLNDpKWTuPGmRGGKJK;";
 
             services.AddDbContext<FootstepDbContext>(config =>
                 config.UseNpgsql(conectionString));
