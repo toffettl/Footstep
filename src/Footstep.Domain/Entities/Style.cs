5c4e4d30112e4bbbe025749eeb341f7fc796a7a0
aluno <etecae023@hotmail.com>
2025-07-31 08:36:43 -0300

    feat: add Style entity

---
+++ src/Footstep.Domain/Entities/Style.cs
@@ -0,0 +1,16 @@
+﻿using Footstep.Domain.Enums;
+
+namespace Footstep.Domain.Entities
+{
+    public class Style
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public StyleType StyleType { get; set; }
+
+        public ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
