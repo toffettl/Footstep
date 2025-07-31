37b52d31244e890ecde6e1175c4667e7697745f4
aluno <etecae023@hotmail.com>
2025-07-31 11:56:12 -0300

    feat: add StyleType enum

---
+++ src/Footstep.Communication/Enums/StyleType.cs
@@ -0,0 +1,14 @@
+﻿namespace Footstep.Communication.Enums
+{
+    public enum StyleType
+    {
+        Profile = 0,
+        Map = 1,
+        PointOfInterest = 2,
+        Head = 3,
+        Torso = 4,
+        Leg = 5,
+        Bag = 6,
+        Accessory = 7
+    }
+}
