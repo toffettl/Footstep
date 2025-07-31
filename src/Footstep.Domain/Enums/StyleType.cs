c05c162a96728abadc3fae45f50816a39c557d87
aluno <etecae023@hotmail.com>
2025-07-31 08:36:33 -0300

    feat: add StyleType

---
+++ src/Footstep.Domain/Enums/StyleType.cs
@@ -0,0 +1,13 @@
+﻿namespace Footstep.Domain.Enums
+{
+    public enum StyleType
+    {
+        Map = 0,
+        PointOfInterest = 1,
+        Head = 2,
+        Torso = 3,
+        Leg = 4,
+        Bag = 5,
+        Accessory = 6
+    }
+}
