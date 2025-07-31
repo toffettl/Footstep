24809e00a4837a8d4177ad36ccabad1ac697be38
aluno <etecae023@hotmail.com>
2025-07-31 11:55:08 -0300

    feat: add StyleRepository

---
+++ src/Footstep.Infrastructure/DataAccess/Repositories/StyleRepository.cs
@@ -0,0 +1,20 @@
+﻿using Footstep.Domain.Entities;
+using Footstep.Domain.Repositories.Styles;
+
+namespace Footstep.Infrastructure.DataAccess.Repositories
+{
+    public class StyleRepository : IStyleWriteOnlyRepository
+    {
+        private readonly FootstepDbContext _dbContext;
+
+        public StyleRepository(FootstepDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(Style style)
+        {
+            await _dbContext.Styles.AddAsync(style);
+        }
+    }
+}
