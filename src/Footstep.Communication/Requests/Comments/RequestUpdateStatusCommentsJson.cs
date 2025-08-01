87e6c4775260cc3cf0d1fa22342ada428627620e
aluno <etecae023@hotmail.com>
2025-08-01 11:58:24 -0300

    in-prod: add a trataement in CommentStatus

--- src/Footstep.Communication/Requests/Comments/RequestUpdateStatusCommentsJson.cs
+++ src/Footstep.Communication/Requests/Comments/RequestUpdateStatusCommentsJson.cs
@@ -2,7 +2,17 @@
 {
     public class RequestUpdateStatusCommentsJson
     {
+        public RequestUpdateStatusLikeCommentsJson Likes { get; set; }
+        public RequestUpdateStatusReplieCommentsJson Replies { get; set; }
+    }
+
+    public class RequestUpdateStatusLikeCommentsJson
+    {
         public int Likes { get; set; }
+    }
+
+    public class RequestUpdateStatusReplieCommentsJson
+    {
         public int Replies { get; set; }
     }
 }
