42cb5adb96eb86cd576ce88612b7dd003a45bcf8
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-01 11:17:21 -0300

    feat: add ResponseStyleJson

---
+++ src/Footstep.Communication/Responses/Styles/ResponseStyleJson.cs
@@ -0,0 +1,14 @@
+﻿using Footstep.Communication.Enums;
+
+namespace Footstep.Communication.Responses.Styles
+{
+    public class ResponseStyleJson
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public StyleType StyleType { get; set; }
+    }
+}
