69bfe6134387f68cda24f826a1b5dc6f9e7ced28
aluno <etecae023@hotmail.com>
2025-08-01 11:10:16 -0300

    feat: add IStyleReadOnlyRepository interface in StyleRepository

--- src/Footstep.Infrastructure/DataAccess/Repositories/StyleRepository.cs
+++ src/Footstep.Infrastructure/DataAccess/Repositories/StyleRepository.cs
@@ -1,9 +1,10 @@
 ﻿using Footstep.Domain.Entities;
 using Footstep.Domain.Repositories.Styles;
+using Microsoft.EntityFrameworkCore;
 
 namespace Footstep.Infrastructure.DataAccess.Repositories
 {
-    public class StyleRepository : IStyleWriteOnlyRepository
+    public class StyleRepository : IStyleWriteOnlyRepository, IStyleReadOnlyRepository
     {
         private readonly FootstepDbContext _dbContext;
 
@@ -15,6 +16,11 @@ namespace Footstep.Infrastructure.DataAccess.Repositories
         public async Task Add(Style style)
         {
             await _dbContext.Styles.AddAsync(style);
+        }
+
+        public async Task<bool> ExistActiveStyleWithName(string name)
+        {
+            return await _dbContext.Styles.AnyAsync(style => style.Name!.Equals(name));
         }
     }
 }
