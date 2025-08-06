dd032605caa5af7244e07aea4544bf2dd6e1f20c
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-06 11:56:29 -0300

    feat: add Create function in StyleController

---
+++ src/Footstep.Api/Controllers/StyleController.cs
@@ -0,0 +1,24 @@
+﻿using Footstep.Application.UseCases.Styles.Create;
+using Footstep.Communication.Requests.Styles;
+using Footstep.Communication.Responses;
+using Footstep.Communication.Responses.Styles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Footstep.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StyleController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseStyleJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Create([FromServices] ICreateStyleUseCase usecase,
+            [FromBody] RequestStyleJson request)
+        {
+            var response = await usecase.Execute(request);
+
+            return Ok(response);
+        }
+    }
+}
