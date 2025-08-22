18d6ed664af4fd6e4e604d23e4d8ff1f9aab4969
aluno <etecae023@hotmail.com>
2025-08-22 11:38:45 -0300

    feat: update user entity

--- src/Footstep.Domain/Entities/User.cs
+++ src/Footstep.Domain/Entities/User.cs
@@ -6,21 +6,6 @@ public class User
     public string? Email { get; set; }
     public string? Biography { get; set; }
     public string? Password { get; set; }
-    public string? MapStyle { get; set; }
-    public string? PointOfInterestStyle { get; set; }
-    public bool? AvatarOverProfile { get; set; }
-    public string? HeadStyle { get; set; }
-    public string? TorsoStyle { get; set; }
-    public string? LegStyle { get; set; }
-    public string? BagStyle { get; set; }
-    public string? AcessoryStyle {  get; set; }
-    public string? UnlockedMapStyles { get; set; }
-    public string? UnlockedPointOfInterestStyles { get; set; }
-    public string? UnlockedHeadStyles { get; set; }
-    public string? UnlockedTorsoStyles { get; set; }
-    public string? UnlockedLegStyles { get; set; }
-    public string? UnlockedBagStyles { get; set; }
-    public string? UnlockedAcessoryStyles { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 
