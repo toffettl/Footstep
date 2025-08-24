16b15fa5276f8fa828229f8d0daacc5e4b0b1b2b
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-24 09:58:19 -0300

    feat: add TypeValidator

---
+++ src/Footstep.Application/UseCases/Comments/TypeValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Footstep.Exception;
+
+namespace Footstep.Application.UseCases.Comments
+{
+    public class TypeValidator : AbstractValidator<int>
+    {
+        public TypeValidator()
+        {
+            RuleFor(x => x)
+                .InclusiveBetween(0, 1).WithMessage(ResourceErrorMessages.TYPE_INVALID);
+        }
+    }
+}
