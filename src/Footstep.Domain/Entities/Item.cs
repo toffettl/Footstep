dadd70782f84b7b9fafe5128bf37a83b6645c2f5
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-24 08:47:56 -0300

    feat: add Item entity

---
+++ src/Footstep.Domain/Entities/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace Footstep.Domain.Entities
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+
+        public bool Unblocked { get; set; }
+        public bool Equipped { get; set; }
+
+        public Guid PreferenceId { get; set; }
+        public Guid StyleId { get; set; }
+
+        public Preference? Preference { get; set; }
+        public Style? Style { get; set; }
+    }
+}
