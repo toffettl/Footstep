9e3576f0be923b151b05b42df45e7666dcc19d52
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-24 08:48:30 -0300

    feat: add Style entity

---
+++ src/Footstep.Domain/Entities/Style.cs
@@ -0,0 +1,17 @@
+﻿using Footstep.Domain.Enums;
+
+namespace Footstep.Domain.Entities
+{
+    public class Style
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public bool Store { get; set; }
+
+        public StyleType StyleType { get; set; }
+
+        public ICollection<Item> Items { get; set; } = new List<Item>();
+    }
+}
