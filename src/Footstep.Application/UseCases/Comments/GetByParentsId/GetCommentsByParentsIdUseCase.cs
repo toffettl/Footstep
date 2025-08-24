75c653c078541c7621942d7efdb8cc77ae424824
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-24 10:04:32 -0300

    feat: add type property parameter to Execute function in IGetCommentsByParentsIdUseCase

--- src/Footstep.Application/UseCases/Comments/GetByParentsId/GetCommentsByParentsIdUseCase.cs
+++ src/Footstep.Application/UseCases/Comments/GetByParentsId/GetCommentsByParentsIdUseCase.cs
@@ -17,15 +17,34 @@ namespace Footstep.Application.UseCases.Comments.GetByParentsId
             _mapper = mapper;
         }
 
-        public async Task<List<ResponseCommentJson>> Execute(Guid id)
+        public async Task<List<ResponseCommentJson>> Execute(Guid id, int type)
         {
-            var result = await _repository.GetByParentsId(id);
+            var result = await _repository.GetByPointOfInterestId(id);
+
+            if (type == 1)
+            {
+                result = await _repository.GetByCommentId(id);
+            }
 
             if (result.Count == 0)
             {
                 throw new NotFoundException(ResourceErrorMessages.COMMENT_NOT_FOUND);
             }
             return _mapper.Map<List<ResponseCommentJson>>(result);
+        }
+
+        private void Validate(int type)
+        {
+            var validator = new TypeValidator();
+
+            var result = validator.Validate(type);
+
+            if (result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(errorMessages);
+            }
         }
     }
 }
