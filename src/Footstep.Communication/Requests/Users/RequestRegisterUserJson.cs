0734f2d4a996061c1464050a002577795848ff08
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-25 04:37:19 -0300

    feat: add MapStyle property to RequestRegisterUserJson

--- src/Footstep.Communication/Requests/Users/RequestRegisterUserJson.cs
+++ src/Footstep.Communication/Requests/Users/RequestRegisterUserJson.cs
@@ -5,4 +5,5 @@ public class RequestRegisterUserJson
     public string? Email { get; set; }
     public string? Password { get; set; }
     public string? Biography { get; set; }
+    public string? MapStyle { get; set; }
 }
