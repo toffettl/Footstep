531ac5ce8e3caf12477c6910c0eca4056960ecb0
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-25 02:18:06 -0300

    feat: add IItemReadOnlyRepository interface

---
+++ src/Footstep.Domain/Repositories/Items/IItemReadOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿using Footstep.Domain.Entities;
+
+namespace Footstep.Domain.Repositories.Items
+{
+    public interface IItemReadOnlyRepository
+    {
+        Task<Item?> GetByPreferenceIdAndStyleId(Guid preferenceId, Guid styleId);
+    }
+}
