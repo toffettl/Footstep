3ab5d8e47929561fdd6e1b116074cea9786a2259
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-25 02:21:53 -0300

    feat: add StyleType enum

---
+++ src/Footstep.Communication/Enums/StyleType.cs
@@ -0,0 +1,11 @@
+﻿namespace Footstep.Communication.Enums
+{
+    public enum StyleType
+    {
+        Head = 0,
+        Body = 1,
+        Legs = 2,
+        Bag = 3,
+        Accessory = 4
+    }
+}
