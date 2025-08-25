eaafb48c3d1213de13bc89a8ad2e7b8580891166
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-25 02:15:18 -0300

    feat: add Add and GetAll functions to PreferenceRepository

---
+++ src/Footstep.Infrastructure/DataAccess/Repositories/PreferenceRepository.cs
@@ -0,0 +1,26 @@
+﻿using Footstep.Domain.Entities;
+using Footstep.Domain.Repositories.Preferences;
+using Microsoft.EntityFrameworkCore;
+
+namespace Footstep.Infrastructure.DataAccess.Repositories
+{
+    public class PreferenceRepository : IPreferenceWriteOnlyRepository, IPreferenceReadOnlyRepository
+    {
+        private readonly FootstepDbContext _dbContext;
+
+        public PreferenceRepository(FootstepDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(Preference preference)
+        {
+            await _dbContext.Preferences.AddAsync(preference);
+        }
+
+        public async Task<List<Preference>> GetAll()
+        {
+            return await _dbContext.Preferences.AsNoTracking().ToListAsync();
+        }
+    }
+}
