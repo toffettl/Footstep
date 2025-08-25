9a9d139876eb107e5b54d383ff7922a22322091b
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-25 02:17:15 -0300

    feat: add Add and GetByName functions to StyleRepository

---
+++ src/Footstep.Infrastructure/DataAccess/Repositories/StyleRepository.cs
@@ -0,0 +1,26 @@
+﻿using Footstep.Domain.Entities;
+using Footstep.Domain.Repositories.Styles;
+using Microsoft.EntityFrameworkCore;
+
+namespace Footstep.Infrastructure.DataAccess.Repositories
+{
+    public class StyleRepository : IStyleWriteOnlyRepository, IStyleReadOnlyRepository
+    {
+        private readonly FootstepDbContext _dbContext;
+
+        public StyleRepository(FootstepDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(Style style)
+        {
+            await _dbContext.Styles.AddAsync(style);
+        }
+
+        public async Task<Style?> GetByName(string name)
+        {
+            return await _dbContext.Styles.AsNoTracking().FirstOrDefaultAsync(style => style.Name == name);
+        }
+    }
+}
