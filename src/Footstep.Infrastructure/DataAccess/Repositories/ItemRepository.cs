1cd8e2a33b5f41526e7051e9bd15cc15c6dd3891
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-25 02:18:40 -0300

    feat: add ItemRepository

---
+++ src/Footstep.Infrastructure/DataAccess/Repositories/ItemRepository.cs
@@ -0,0 +1,26 @@
+﻿using Footstep.Domain.Entities;
+using Footstep.Domain.Repositories.Items;
+using Microsoft.EntityFrameworkCore;
+
+namespace Footstep.Infrastructure.DataAccess.Repositories
+{
+    public class ItemRepository : IItemWriteOnlyRepository, IItemReadOnlyRepository
+    {
+        private readonly FootstepDbContext _dbContext;
+
+        public ItemRepository(FootstepDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(Item item)
+        {
+            await _dbContext.Items.AddAsync(item);
+        }
+
+        public async Task<Item?> GetByPreferenceIdAndStyleId(Guid preferenceId, Guid styleId)
+        {
+            return await _dbContext.Items.AsNoTracking().FirstOrDefaultAsync(item => item.PreferenceId == preferenceId && item.StyleId == styleId);
+        }
+    }
+}
