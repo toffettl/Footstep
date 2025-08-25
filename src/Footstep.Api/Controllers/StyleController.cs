7993544142509e791a28b63d4a9c16e5f22637a1
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-25 02:27:50 -0300

    feat: add StyleController

---
+++ src/Footstep.Api/Controllers/StyleController.cs
@@ -0,0 +1,25 @@
+﻿using Footstep.Application.UseCases.Styles.Create;
+using Footstep.Communication.Requests.Styles;
+using Footstep.Communication.Responses;
+using Footstep.Communication.Responses.Styles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Footstep.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StyleController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseStyleJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateStyle(
+            [FromServices] ICreateStyleUseCase useCase,
+            [FromBody] RequestStyleJson request)
+        {
+            var response = await useCase.Execute(request);
+
+            return Ok(response);
+        }
+    }
+}
