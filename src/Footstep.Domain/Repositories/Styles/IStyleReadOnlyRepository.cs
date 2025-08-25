635df64087760b694d6c53a4ddc0f3f4f8c59ad1
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-25 02:16:35 -0300

    feat: add IStyleReadOnlyRepository interface

---
+++ src/Footstep.Domain/Repositories/Styles/IStyleReadOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿using Footstep.Domain.Entities;
+
+namespace Footstep.Domain.Repositories.Styles
+{
+    public interface IStyleReadOnlyRepository
+    {
+        Task<Style?> GetByName(string name);
+    }
+}
