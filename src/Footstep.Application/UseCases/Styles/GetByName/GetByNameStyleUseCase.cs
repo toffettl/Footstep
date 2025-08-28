712c651da59803c9044476de6c1f0d1797de5407
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-27 22:29:17 -0300

    feat: add GetByNameStyleUseCase

---
+++ src/Footstep.Application/UseCases/Styles/GetByName/GetByNameStyleUseCase.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Footstep.Communication.Responses.Styles;
+using Footstep.Domain.Repositories.Styles;
+using Footstep.Exception;
+
+namespace Footstep.Application.UseCases.Styles.GetByName
+{
+    public class GetByNameStyleUseCase : IGetByNameStyleUseCase
+    {
+        private readonly IStyleReadOnlyRepository _styleReadOnlyRepository;
+        private readonly IMapper _mapper;
+
+        public GetByNameStyleUseCase(
+            IStyleReadOnlyRepository styleReadOnlyRepository,
+            IMapper mapper)
+        {
+            _styleReadOnlyRepository = styleReadOnlyRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseStyleJson> Execute(string name)
+        {
+            var style = await _styleReadOnlyRepository.GetByName(name);
+
+            if (style == null)
+            {
+                throw new DirectoryNotFoundException(ResourceErrorMessages.STYLE_NOT_FOUND);
+            }
+
+            return _mapper.Map<ResponseStyleJson>(style);
+        }
+    }
+}
