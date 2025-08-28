aa90fb1fc8639fbae090fb0549bb962f90268773
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-28 14:03:46 -0300

    feat: add IAddressReadOnlyRepository interface

---
+++ src/Footstep.Domain/Repositories/Addresses/IAddressReadOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿using Footstep.Domain.Entities;
+
+namespace Footstep.Domain.Repositories.Addresses
+{
+    public interface IAddressReadOnlyRepository
+    {
+        Task<Address?> GetByLatitudeAndLongitude(double latitude, double longitude);
+    }
+}
