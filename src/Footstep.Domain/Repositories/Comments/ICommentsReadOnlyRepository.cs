3255155c41f0612689b67cbf5092a1ebf6cdd249
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-30 03:58:39 -0300

    feat: add ICommentLikeWriteOnlyRepository interface

--- src/Footstep.Domain/Repositories/Comments/ICommentsReadOnlyRepository.cs
+++ src/Footstep.Domain/Repositories/Comments/ICommentsReadOnlyRepository.cs
@@ -4,6 +4,7 @@ namespace Footstep.Domain.Repositories.Comments
 {
     public interface ICommentsReadOnlyRepository
     {
+        Task<Comment?> GetById(Guid id);
         Task<List<Comment>> GetByPointOfInterestId(Guid id);
         Task<List<Comment>> GetByCommentId(Guid id);
         Task<List<Comment>> GetByUserId(Guid id);
--- src/Footstep.Domain/Repositories/Comments/ICommentsUpdateOnlyRepository.cs
+++ src/Footstep.Domain/Repositories/Comments/ICommentsUpdateOnlyRepository.cs
@@ -4,7 +4,6 @@ namespace Footstep.Domain.Repositories.Comments
 {
     public interface ICommentsUpdateOnlyRepository
     {
-        Task<Comment?> GetById(Guid id);
         void Update(Comment comment);
     }
 }
