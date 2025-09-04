fd147eba8753aae4aa0a11fad4ffb5ef7922a61f
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-09-04 17:26:06 -0300

    feat: GetAllPointsOfInterestByPageUseCase

---
+++ src/Footstep.Application/UseCases/Traces/GetAllByPage/GetAllPointsOfInterestByPageUseCase.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Footstep.Communication.Responses;
+using Footstep.Communication.Responses.Traces;
+using Footstep.Domain.Repositories.Traces;
+using Footstep.Exception;
+
+namespace Footstep.Application.UseCases.Traces.GetAllByPage
+{
+    public class GetAllPointsOfInterestByPageUseCase : IGetAllPointsOfInterestByPageUseCase
+    {
+        private readonly IPointOfInterestReadOnlyRepository _pointsOfInterestReadOnlyRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllPointsOfInterestByPageUseCase(
+            IPointOfInterestReadOnlyRepository pointsOfInterestReadOnlyRepository,
+            IMapper mapper)
+        {
+            _pointsOfInterestReadOnlyRepository = pointsOfInterestReadOnlyRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<PagedResult<ResponsePointOfInterestJson>> Execute(int page, int pageSize)
+        {
+            var (pointsOfInterest, totalCount) = await _pointsOfInterestReadOnlyRepository.GetAllByPage(page, pageSize);
+
+            var responses = _mapper.Map<List<ResponsePointOfInterestJson>>(pointsOfInterest);
+
+            return new PagedResult<ResponsePointOfInterestJson>
+            {
+                Items = responses,
+                Page = page,
+                PageSize = pageSize,
+                TotalCount = totalCount,
+                TotalPages = (int)Math.Ceiling((double)totalCount / pageSize)
+            };
+        }
+    }
+}
