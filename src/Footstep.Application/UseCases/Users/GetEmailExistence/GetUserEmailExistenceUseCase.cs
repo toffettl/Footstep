6a958a4962c941a4e37f59c37e16c7e67f4d745e
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-10-05 21:09:48 -0300

    feat: add GetUserEmailExistenceUseCase

---
+++ src/Footstep.Application/UseCases/Users/GetEmailExistence/GetUserEmailExistenceUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Footstep.Domain.Repositories.Users;
+
+namespace Footstep.Application.UseCases.Users.GetEmailExistence
+{
+    public class GetUserEmailExistenceUseCase : IGetUserEmailExistenceUseCase
+    {
+        private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+
+        public GetUserEmailExistenceUseCase(IUserReadOnlyRepository userReadOnlyRepository)
+        {
+            _userReadOnlyRepository = userReadOnlyRepository;
+        }
+
+        public async Task<bool> Execute(string email)
+        {
+            return await _userReadOnlyRepository.ExistActiveUserWithEmail(email);
+        }
+    }
+}
