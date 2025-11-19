e9cef26f6b37160e1fe047b1730dd26e1684cf88
aluno <etecae023@hotmail.com>
2025-11-19 10:22:01 -0300

    feat: add Image entity

---
+++ src/Footstep.Domain/Entities/Image.cs
@@ -0,0 +1,12 @@
+﻿namespace Footstep.Domain.Entities
+{
+    public class Image
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Url { get; set; }
+
+        public Guid PointOfInterestId { get; set; }
+
+        public PointOfInterest? PointOfInterest { get; set; }
+    }
+}
