aa34e8aa1b2f75201f6365632c93aec1d6400205
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-11-20 22:42:12 -0300

    feat: add IUpdateAddImagePointOfInterestUseCase interface

---
+++ src/Footstep.Application/UseCases/PointsOfInterest/UpdateAddImage/IUpdateAddImagePointOfInterestUseCase.cs
@@ -0,0 +1,7 @@
+﻿namespace Footstep.Application.UseCases.PointsOfInterest.UpdateImages
+{
+    public interface IUpdateAddImagePointOfInterestUseCase
+    {
+        Task Execute(Guid pointOfInterestId, Stream stream, string fileName, string contentType);
+    }
+}
